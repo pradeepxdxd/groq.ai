ba153e4251dcdbdbba1a917362016451f89edd76
pradeepxdxd <pradeepbiswas41813@gmail.com>
2024-11-19 20:22:13 +0530

    fix production cors issue

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ import cors from 'cors';
 import router from './route.js';
 
 const app = express();
-app.use(cors())
+app.use(cors({
+    origin: process.env.ASK_AI_BASE_URL || 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 
